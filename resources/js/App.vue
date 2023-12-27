d62c49804f6bce718e73d23839a0a0cffdda9cee
simon <simongouzien@hotmail.fr>
2023-12-27 15:01:08 +0100

    add project

---
+++ resources/js/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="wrapper">
+    <!-- sidebar -->
+    <div class="sidebar">
+      <span class="closeButton">&times;</span>
+      <p class="brand-title"><a href="">simon laravel vue Blog</a></p>
+
+      <div class="side-links">
+        <ul>
+          <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
+          <li><router-link :to="{ name: 'Blog' }">Blog</router-link></li>
+          <li><router-link :to="{ name: 'About' }">About</router-link></li>
+          <li><router-link :to="{ name: 'Contact' }">Contact</router-link></li>
+        </ul>
+      </div>
+
+      <!-- sidebar footer -->
+      <footer class="sidebar-footer">
+        <div>
+          <a href=""><i class="fab fa-facebook-f"></i></a>
+          <a href=""><i class="fab fa-instagram"></i></a>
+          <a href=""><i class="fab fa-twitter"></i></a>
+        </div>
+
+        <small>&copy; 2023 Simon Blog</small>
+      </footer>
+    </div>
+    <!-- Menu Button -->
+    <div class="menuButton">
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    </div>
+    <!-- main -->
+    <main class="container">
+      <!-- render components depending on the page visited -->
+
+      <router-view></router-view>
+    </main>
+
+    <!-- Main footer -->
+    <footer class="main-footer">
+      <div>
+        <a href=""><i class="fab fa-facebook-f"></i></a>
+        <a href=""><i class="fab fa-instagram"></i></a>
+        <a href=""><i class="fab fa-twitter"></i></a>
+      </div>
+      <small>&copy; 2023 simon Blog</small>
+    </footer>
+  </div>
+</template>
